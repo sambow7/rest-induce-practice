54a7b90ee8f7293d806fda8e2b86f18222ed4284
Sam Bowen <samhbowen@gmail.com>
2024-12-08 20:05:50 -0800

    add .env /books rename

--- controllers/book.js
+++ controllers/book.js
@@ -1,96 +1,65 @@
-const books = require('../data/books');
+const books = require('../data/books')
 
 
-// Index
-app.get('/', (req, res) => {
-    res.render('home', { title: 'Library' });
-});
-app.get('/books', (req, res) => {
-    res.render('book', { title: "Book List", books: books });
-});
+function index(req, res) {
+    res.render('books', { title: "Book List", books })
+}
 
-app.get('/books/new', (req, res) => {
-    res.render('book/new.ejs', { title: 'New Book' });
-});
 
-app.get('/books/:id', (req, res) => {
-    const book = books.find(book => book.id === Number(req.params.id));
-    if (book) {
-        res.render('book/show', { title: book.title, book: book });
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' });
-    }
-});
+function newBook(req, res) {
+    res.render('books/new', { title: 'New Book' })
+}
 
-// New
-app.post('/books/new', (req, res) => {
-    const newBook = { id: books.length + 1, title: 'New Book', author: 'New Author' };
-    books.push(newBook);
-    console.log(books);
-    const response = `New book created: ${newBook.title} by ${newBook.author}`;
-    res.send(response);
-})
-
-// app.post('/seed', async (req, res) => {
-//     try {
-//         await Book.insertMany(books)
-//         res.status(201).send('books seeding')
-//     } catch (error) {
-//         console.log(error.message)
-//         res.status(404).send('error seeding bok')
-//     }
-// });
-
-// Delete
-app.delete('/books/:id', (req, res) => {
-    const bookId = parseInt(req.params.id);
-    const bookIdx = books.findIndex(book => book.id === bookId);
-    if (bookIdx !== -1) {
-        books.splice(bookIdx, 1);
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' });
-    }
-    res.status(200).redirect('/books');
-})
-
-// Update
-app.put('/books/:id', (req, res) => {
-    const bookId = parseInt(req.params.id);
-    const bookIndex = books.findIndex(book => book.id === bookId)
-    console.log(bookIndex)
-    if (bookIndex !== -1) {
-        for (const key in req.body) {
-            books[bookIndex][key] = req.body[key];
-        }
-        res.status(200).redirect('/books')
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' })
-    }
-})
-
-// Create
-app.post('/books', (req, res) => {
-    const newBook = { 
+function postBook(req, res) {
+    const newBook = {
         id: books.length + 1,
-        title: req.body.title, 
-        author: req.body.author,
-        published: req.body.published || '',
-        description: req.body.description || '',
-    };
-    if (newBook.title && newBook.author) {
-        books.push(newBook);
-        res.status(201).redirect('/books');
-    } else {
-        res.status(400).json({ message: 'Invalid book' });
+        title: req.body.title || "new book",
+        author: req.body.author || "new author"
     }
-})
+    books.push(newBook);
+    res.status(201).redirect('/books')
+}
 
-// Edit
-app.get('/books/:id/edit', (req, res) => {
-    const book = books.find(book => book.id === Number(req.params.id));
+function showBook(req, res) {
+    const book = books.find(book => book.id === parseInt(req.params.id));
     if (book) {
-        res.render('book/edit', { title: 'Edit Book', book: book });
+        res.render('books/show', { title: 'Book Details', book })
+    } else {
+        res.status(404).render('404/notFound', { title: "Book not found" })
+    }
+}
+
+
+function editBook(req, res) {
+    const book = books.find(book => book.id === parseInt(req.params.id));
+    if (book) {
+        res.render('books/edit', { title: 'Edit Book', book });
+    } else {
+        res.status(404).render('404/notFound', { title: 'Book Not Found!' })
+    }
+}
+
+function updateBook(req, res) {
+    const bookId = parseInt(req.params.id);
+    const bookIndex = books.findIndex(book => book.id === bookId);
+    if (bookIndex !== -1) {
+        books[bookIndex] = { ...books[bookIndex], ...req.body };
+        res.status(200).redirect(`/books`);
+        // res.render('bookUpdated', { title: 'Book Updated', book: books[bookIndex] });
     } else {
         res.status(404).render('404/notFound', { title: 'Book Not Found' });
     }
-})
+}
+
+function deleteBook(req, res) {
+    const bookId = parseInt(req.params.id);
+    const bookIndex = books.findIndex(book => book.id === bookId);
+    if (bookIndex !== -1) {
+        books.splice(bookIndex, 1);
+    } else {
+        res.status(404).render('404/notFound', { title: 'Book Not Found' });
+    }
+    res.redirect('/books');
+}
+
+module.exports = { index, newBook, postBook, editBook, updateBook, showBook, deleteBook }
--- server.js
+++ server.js
@@ -1,135 +1,49 @@
-const dotenv = require('dotenv')
-dotenv.config();
-const methodOverride = require('method-override');
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
-const books = require('./data/books.js');
+const methodOverride = require('method-override')
 const app = express();
-const mongoose = require('mongoose');
-const Book = require('./models/book.js');
-const PORT = 3001;
+const path = require('path');
 
-mongoose.connect(process.env.MONGODB_URI, {
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.log(err);
-})
+
+// ==================
+// CONFIGURE MONGOOSE
+// ==================
+require('./configs/database');
+
+
+// ********************
+//    MIDDLEWARE
+// ********************
 
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(methodOverride('_method'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(methodOverride('_method'))
 
+// Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'public')))
 
-// I.N.D.U.C.E.
+// ***************************
+//      ROUTES 
+// ***************************
 
-//Seed Routes
-app.use('/', require('./routes/seed.js'));
+// Seed Route
+app.use('/', require('./routes/seed'));
 
-//Home Route 
-app.use('/', require('./routes/home.js'));
+// Home Route
+app.use('/', require('./routes/home'));
+
+// Book Routes
+app.use('/', require('./routes/book'));
+
+// ********************
+//    LISTENNER
+// ********************
 
 
-// Index
-app.get('/', (req, res) => {
-    res.render('home', { title: 'Library' });
-});
-app.get('/books', (req, res) => {
-    res.render('book', { title: "Book List", books: books });
-});
-
-app.get('/books/new', (req, res) => {
-    res.render('book/new.ejs', { title: 'New Book' });
-});
-
-app.get('/books/:id', (req, res) => {
-    const book = books.find(book => book.id === Number(req.params.id));
-    if (book) {
-        res.render('book/show', { title: book.title, book: book });
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' });
-    }
-});
-
-// New
-app.post('/books/new', (req, res) => {
-    const newBook = { id: books.length + 1, title: 'New Book', author: 'New Author' };
-    books.push(newBook);
-    console.log(books);
-    const response = `New book created: ${newBook.title} by ${newBook.author}`;
-    res.send(response);
+app.listen(process.env.PORT, () => {
+    console.log(`🎧 Server is running on http://localhost:${process.env.PORT}`);
 })
-
-// app.post('/seed', async (req, res) => {
-//     try {
-//         await Book.insertMany(books)
-//         res.status(201).send('books seeding')
-//     } catch (error) {
-//         console.log(error.message)
-//         res.status(404).send('error seeding bok')
-//     }
-// });
-
-// Delete
-app.delete('/books/:id', (req, res) => {
-    const bookId = parseInt(req.params.id);
-    const bookIdx = books.findIndex(book => book.id === bookId);
-    if (bookIdx !== -1) {
-        books.splice(bookIdx, 1);
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' });
-    }
-    res.status(200).redirect('/books');
-})
-
-// Update
-app.put('/books/:id', (req, res) => {
-    const bookId = parseInt(req.params.id);
-    const bookIndex = books.findIndex(book => book.id === bookId)
-    console.log(bookIndex)
-    if (bookIndex !== -1) {
-        for (const key in req.body) {
-            books[bookIndex][key] = req.body[key];
-        }
-        res.status(200).redirect('/books')
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' })
-    }
-})
-
-// Create
-app.post('/books', (req, res) => {
-    const newBook = { 
-        id: books.length + 1,
-        title: req.body.title, 
-        author: req.body.author,
-        published: req.body.published || '',
-        description: req.body.description || '',
-    };
-    if (newBook.title && newBook.author) {
-        books.push(newBook);
-        res.status(201).redirect('/books');
-    } else {
-        res.status(400).json({ message: 'Invalid book' });
-    }
-})
-
-// Edit
-app.get('/books/:id/edit', (req, res) => {
-    const book = books.find(book => book.id === Number(req.params.id));
-    if (book) {
-        res.render('book/edit', { title: 'Edit Book', book: book });
-    } else {
-        res.status(404).render('404/notFound', { title: 'Book Not Found' });
-    }
-});
-
-//~~~~~~~~~~~
-// LISTENER
-//~~~~~~~~~~~~
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
